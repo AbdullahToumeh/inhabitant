a812c6016244b45724bbc14ba8a79c290ffcd8c9
Dakota M <dakota.paige@hotmail.com>
2018-02-23 16:06:37 -0800

    Added styling for forms and search in header

--- themes/inhabitant/header.php
+++ themes/inhabitant/header.php
@@ -23,13 +23,14 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<img src="<?php echo get_template_directory_uri() . '/images/inhabitent-logo-tent.svg'; ?>" class="logo" alt="Inhabitent logo" />
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri() . '/images/inhabitent-logo-tent.svg'; ?>" class="logo" alt="Inhabitent logo" /></a>
 					<!-- <p class="site-description"><?php bloginfo( 'description' ); ?></p> -->
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<div class="header-search"><?php get_search_form(); ?></div>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
