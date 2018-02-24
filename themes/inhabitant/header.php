473cc7571f5db2ee3eed6769b37889e3633e957a
Dakota M <dakota.paige@hotmail.com>
2018-02-24 11:24:30 -0800

    Added search bar to header

--- themes/inhabitant/header.php
+++ themes/inhabitant/header.php
@@ -30,6 +30,7 @@
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<i class="fas fa-search"></i>
 					<div class="header-search"><?php get_search_form(); ?></div>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
