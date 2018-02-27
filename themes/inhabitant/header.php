23ee985d430454a1b3043b5ff63dda6689ebacb9
Dakota M <dakota.paige@hotmail.com>
2018-02-26 16:25:25 -0800

    Fixed logo change for header on certain pages

--- themes/inhabitant/header.php
+++ themes/inhabitant/header.php
@@ -23,7 +23,7 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri() . '/images/inhabitent-logo-tent.svg'; ?>" class="logo" alt="Inhabitent logo" /></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><div class="tent-logo"></div></a>
 					<!-- <p class="site-description"><?php bloginfo( 'description' ); ?></p> -->
 				</div><!-- .site-branding -->
 
