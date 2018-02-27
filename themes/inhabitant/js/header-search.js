ab1eb3d799b90b6b8355a158c11d98c8be880a79
Dakota M <dakota.paige@hotmail.com>
2018-02-26 16:10:21 -0800

    Added JS functionality for header change on front & about pages

--- themes/inhabitant/js/header-search.js
+++ themes/inhabitant/js/header-search.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
     $('.search-field').focus();
   })
 
+  //if on the home page or about page, add class to header right away
+  if ($('.home').length || $('.page-template-about').length) {
+    $('.site-header').addClass('hero-page-header');
+  }
+
+  //function to handle the change of header when user scrolls past the height of the header image
+  $(window).scroll(function(){
+    var currentScreenPosition  = $(document).scrollTop();
+    if (currentScreenPosition < $('.front-header').height() || currentScreenPosition < $('.page-template-about .entry-header').height()) {
+      $( '.site-header' ).addClass( 'hero-page-header' );
+    } else {
+      $( '.site-header' ).removeClass( 'hero-page-header' );
+    }
+  });
 })
