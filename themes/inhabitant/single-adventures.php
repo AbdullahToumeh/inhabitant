57db155b520d651890adf384b8a5251ab091e0ee
Dakota M <dakota.paige@hotmail.com>
2018-03-08 09:42:54 -0800

    Built out adventures CPT and edited the loop on single-adventures page

---
+++ themes/inhabitant/single-adventures.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Inhabitent
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+      <header class="adventure-header">
+				<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?>
+				<?php endif; ?>
+			</header><!-- .entry-header -->
+
+      <div class="adventure-content">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          <?php the_author(); ?>
+					<?php the_content(); ?>
+
+			<button class="social-media-button"><i class="fab fa-facebook-f"></i> &nbsp;Like</button>
+			<button class="social-media-button"><i class="fab fa-twitter"></i> &nbsp;Tweet</button>
+			<button class="social-media-button"><i class="fab fa-pinterest"></i> &nbsp;Pin</button>
+
+      </div><!-- .adventure-content -->
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
